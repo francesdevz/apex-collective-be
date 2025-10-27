21a87e0a0d8c9a91fc5cfb140dadeea2b00309e2
francesdevz <dfrancesdonz@yahoo.com>
2025-10-27 15:00:32 +0800

    Adding external tomcat server

--- src/main/java/ApexCollectiveHibernateBE/ApexCollective/ApexCollectiveApplication.java
+++ src/main/java/ApexCollectiveHibernateBE/ApexCollective/ApexCollectiveApplication.java
@@ -2,12 +2,20 @@ package ApexCollectiveHibernateBE.ApexCollective;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ApexCollectiveApplication {
+public class ApexCollectiveApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ApexCollectiveApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApexCollectiveApplication.class, args);
 	}
+
 
 }
